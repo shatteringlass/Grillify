62188eebdaa51f2bfec2f602d8f6fb46dada7700
shatteringlass <federico.pizzolo@gmail.com>
2014-06-11 16:17:48 +0200

    Modificata funzione logica

--- Source/content_script.js
+++ Source/content_script.js
@@ -33,31 +33,14 @@ function handleText(textNode) {
 	var v = textNode.nodeValue;
 
   // Deal with the easy case
-  v = v.replace(/\b(T|t)he (C|c)loud/g, function(match, p1, p2, offset, string) {
+  v = v.replace(/\b(C|C)ondividi/g, function(match, p1, p2, offset, string) {
     // t - 7 = m
     // c - 1 = b
-    m = String.fromCharCode(p1.charCodeAt(0) - 7);
-    b = String.fromCharCode(p2.charCodeAt(0) - 1);
-    return m + "y " + b + "utt";
+    //m = String.fromCharCode(p1.charCodeAt(0) - 7);
+    //b = String.fromCharCode(p2.charCodeAt(0) - 1);
+    return "FAI GIRARE!1!!!1!";
   });
 
-  // Deal with private clouds
-  v = v.replace(/\b(P|p)rivate (C|c)loud/g, function(match, p1, p2, offset, string) {
-    // c - 1 = b
-    b = String.fromCharCode(p2.charCodeAt(0) - 1);
-    return b + "utt";
-  });
-  // Get the corner cases
-  if(v.match(/cloud/i)) {
-    // If we're not talking about weather
-    if(v.match(/PaaS|SaaS|IaaS|computing|data|storage|cluster|distributed|server|hosting|provider|grid|enterprise|provision|apps|hardware|software|/i)) {
-      v = v.replace(/(C|c)loud/gi, function(match, p1, offset, string) {
-        // c - 1 = b
-        b = String.fromCharCode(p1.charCodeAt(0) - 1);
-        return b + "utt";
-      });
-    }
-  }
 	textNode.nodeValue = v;
 }
 
